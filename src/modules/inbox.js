8dd6757c348444478084c513822c97bf84d9c4d0
imtiyazHussain <imtiyaz.hussain@softobiz.com>
2022-07-21 12:52:04 +0530

    first commit

---
+++ src/modules/inbox.js
@@ -0,0 +1,61 @@
+const main = document.getElementById("main");
+const addB = document.getElementById("add");
+var closebtns = document.getElementsByClassName("close");
+// var items = document.getElementsByClassName("item");
+var items = ['hit the gym' , 'pay bills'];
+function closeItem() { for (let i=0; i < closebtns.length; i++) {
+    closebtns[i].addEventListener("click", function() {
+        this.parentElement.style.display = 'none';
+    });
+}};
+
+function checkItem() { 
+    for (let j=0; j < items.length; j++) {
+    items[j].addEventListener("click", function(e) {
+        if (e.target.classList.contains("checked")== true) {
+            this.classList.remove("checked");
+        } else {
+            this.classList.add("checked");
+        };
+    })}
+};
+
+function newItem() {
+    var input = document.getElementById("task");
+    if (input.value === "") {
+        alert("You must write something!");
+    } else {       
+        items.push(input.value);
+        populateItems();
+    };
+
+};
+function addNew() {
+    addB.addEventListener("click", function() {
+    clearDiv(); 
+    newItem();
+    closeItem();
+    });
+};
+
+function clearDiv() {
+    document.getElementById("items").innerHTML="";
+}
+function populateItems() {
+    for (let k=0; k< items.length; k++) {
+        var div = document.createElement("div");
+        var li = document.createElement("li");
+        var span = document.createElement("span");
+        span.classList = "close";
+        var itemT = document.createTextNode(items[k]);
+        li.appendChild(itemT);
+
+        var icon = document.createTextNode("\u00D7");
+        span.appendChild(icon);
+        div.appendChild(li);
+        div.appendChild(span);
+        div.classList = "item";
+        main.appendChild(div);  
+    }
+};
+export {closeItem, checkItem, addNew, populateItems };
---
+++ src/modules/projects.js
@@ -0,0 +1,34 @@
+const sidebar = document.getElementById("sidebar");
+var closeBtns = document.getElementsByClassName("close-pro");
+const addP = document.getElementById("add-pro-btn");
+
+function closePro() { for (let i=0; i < closeBtns.length; i++) {
+    closeBtns[i].addEventListener("click", function() {
+        this.parentElement.style.display = 'none';
+    });
+}};
+var inputP = document.getElementById("project");
+function newItemP() {
+    if (inputP.value === "") {
+        alert("You must write something!");
+    } else {
+        var div = document.createElement("div");
+        var li = document.createElement("li");
+        var span = document.createElement("span");
+        span.classList = "close-pro";
+        var itemP = document.createTextNode(inputP.value);
+        li.appendChild(itemP);
+        var icon = document.createTextNode("\u00D7");
+        span.appendChild(icon);
+        div.appendChild(li);
+        div.appendChild(span);
+        div.classList = "item-pro";
+        sidebar.appendChild(div);
+    };
+};
+function addNewP() {
+    addP.addEventListener("click", function() {
+    newItemP();
+    closePro();
+    })};
+    export {closePro, addNewP};
